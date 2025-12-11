02d57bc92ec869a153fc6fb2359ed2a7e753aea5
Pranav <kumarpranavv.097@gmail.com>
2025-12-11 15:49:14 +0530

    implement Queue using Linked List

---
+++ cs_class/queuelist.c
@@ -0,0 +1,83 @@
+/*
+  Program to implement Queue using Linked List (InitQueue and EnQueue)
+  Pranav
+  9-12-2025
+  queue_enqueue.c
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+
+typedef struct QueueType {
+  int Data;
+  struct QueueType *Next;
+} QUEUENODE;
+
+QUEUENODE *Front, *Rear;
+
+//prototype
+void InitQueue();
+void EnQueue(int num);
+void DisplayQueue();
+
+int main (){
+  int num, n, i;
+
+  InitQueue();
+
+  printf("Program to enter elements in Queue (FIFO)\n");
+  printf("Enter number of elements to be inserted: ");
+  scanf("%d", &n);
+  
+  for(i=0; i<n; i++){
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    EnQueue(num);
+  }
+  
+  DisplayQueue();
+  return 0;
+}
+
+void InitQueue(){
+  Front = NULL;
+  Rear = NULL;
+}
+
+void EnQueue(int num){
+  QUEUENODE *Node;
+  
+  //assigning memory and assigning values
+  Node = (QUEUENODE *)malloc(sizeof(QUEUENODE));
+  Node->Data = num;
+  Node->Next = NULL;
+
+  //case 1: no queue, make the incoming node the front and rear
+  if(Front == NULL){
+    Front = Node;
+    Rear = Node;
+    return;
+  }
+
+  //case 2: attach incoming node to the rear of the queue and make the new node to rear
+  Rear->Next = Node;
+  Rear = Node;
+}
+
+void DisplayQueue(){
+  QUEUENODE *Current;
+  
+  if(Front == NULL){
+    printf("Queue is empty\n");
+    return;
+  }
+  
+  printf("\nQueue Contents:\n");
+  Current = Front;
+  while(Current != NULL){
+    printf("%d -> ", Current->Data);
+    Current = Current->Next;
+  }
+  printf("NULL\n");
+}
