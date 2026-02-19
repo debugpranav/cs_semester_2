265f4e0be28f8234387a715ae5436be08f4a7c50
Pranav <kumarpranavv.097@gmail.com>
2026-02-19 17:37:48 +0530

    finding factorials using stack

---
+++ cs_class/stack_fact.c
@@ -0,0 +1,70 @@
+/* ------------------------------------------------
+   program to find the factorial of a given number
+   Pranav
+   19 Feb, 2026
+   fact_stack.c 
+   ----------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+
+typedef struct StackType {
+    int A[SIZE];
+    int Top;
+} STACK;
+
+STACK S;
+
+void InitStack();
+void Push(int num);
+int Pop();
+
+int main() {
+    int i, N, num, Fact;
+
+    InitStack();
+
+    printf("Enter the Number: ");
+    scanf("%d", &N);
+
+    // Factorial logic using Stack
+    i = 2;
+    Push(1);
+    
+    while(i <= N) {
+        num = Pop();
+        Push(num * i);
+        i++;
+    }
+    
+    Fact = Pop();
+
+    printf("Factorial of %d is: %d\n", N, Fact);
+
+    return 0;
+}
+
+void InitStack() {
+    S.Top = -1;
+}
+
+void Push(int num) {
+    if(S.Top == SIZE - 1) {
+        printf("Stack Overflow.\n");
+        return;
+    }
+    S.Top++;
+    S.A[S.Top] = num;
+}
+
+int Pop() {
+    int num;
+    if(S.Top == -1) {
+        printf("Stack Underflow.\n");
+        return -1;
+    }
+    num = S.A[S.Top];
+    S.Top--;
+    return num;
+}
